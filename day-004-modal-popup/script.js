d6536804db8f04f10f8acc437ce0e01b32d55257
Oniel1721 <oni1san7alv21@gmail.com>
2023-08-16 18:27:11 -0400

    day-004: dialog close when click in backdrop

--- day-004-modal-popup/script.js
+++ day-004-modal-popup/script.js
@@ -10,6 +10,8 @@ const $closeButton = document.querySelector(SELECTORS.close);
 const $dialog = document.querySelector(SELECTORS.dialog);
 const $modal = document.querySelector(SELECTORS.modal);
 
+
+
 const open = () => {
     $dialog.showModal();
 }
@@ -25,10 +27,18 @@ const actions = {
 
 document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (event) => {
-       const actionName = event.target.getAttribute('data-action');
-       if(!actionName) return;
-        const action = actions[actionName];
-        if(!action) return;
-        action();
+        const actionName = event.target.getAttribute('data-action');
+        if (actionName) {
+            const action = actions[actionName];
+            if (!action) return;
+            action();
+        }
+        else if($dialog.open){
+            const x = event.clientX, y = event.clientY;
+            const dialogRect = $dialog.getBoundingClientRect();
+            if(x < dialogRect.left || x > dialogRect.right || y < dialogRect.top || y > dialogRect.bottom){
+                actions.close();
+            }
+        }
     });
 });
